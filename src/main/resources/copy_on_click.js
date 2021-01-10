c3c3025b149b03a9f6481cf4356709ef06a592af
liach <liach@users.noreply.github.com>
2021-01-09 20:44:40 -0600

    Put the copy on click and jd header bakc to mappingpoet. let yarn poll from jar
    Signed-off-by: liach <liach@users.noreply.github.com>

---
+++ src/main/resources/copy_on_click.js
@@ -0,0 +1,17 @@
+document.onreadystatechange = function() {
+  if(document.readyState == "complete") {
+    const items = document.querySelectorAll(".copyable");
+    items.forEach(item => {
+      item.title = "Click to copy";
+      item.style["cursor"] = "pointer";
+      item.onclick = function() {
+        var range = document.createRange();
+        range.selectNode(item);
+        window.getSelection().addRange(range);
+        document.execCommand("copy");
+        window.getSelection().removeRange(range);
+        console.log("Copied to clipboard");
+      };
+    });
+  }
+};
